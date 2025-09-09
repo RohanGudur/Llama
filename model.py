3ee66a718aef30f71fc0d87a53f670621e592e5b
RohanGudur <rohangudur@gmail.com>
2025-09-09 17:55:39 +0530

    added transformer block

--- model.py
+++ model.py
@@ -29,3 +29,41 @@ def RMSnorm(nn.Module):
     def forward(x:torch.Tensor):
         weights = nn.Parameter(torch.ones(dim))
         return weights *  (x * torch.rsqrt(x.pow(2).mean(-1,keepdims = True)+ self.eps))
+
+class Transformer(nn.Module):
+
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+
+        assert args.vocab_size != -1,  "Vocab size must be set"
+        self.args = args
+        
+        #embedding layer
+        self.tok_embeddings = nn.Embedding(args.vocab_size, args.dim)
+        #stacked transfomers blocks
+        self.layers = nn.ModuleList()
+        for layer_id in range(args.n_layers):
+            self.layers.append(EncoderBlock(args))
+        #after final block rms --> linear
+        self.norm = RMSNorm(args.dim, eps=args.norm_eps)
+        self.output = nn.Linear(args.dim, args.vocab_size, bias=False)
+        #--> softmax --> sampling
+        self.freqs_complex = precompute_theta_pos_frequencies(self.args.dim // self.args.n_heads, self.args.max_seq_len * 2, device=self.args.device)
+
+    def forward(self, tokens: torch.Tensor, start_pos: int):
+        # (B, Seq_Len)
+        batch_size, seq_len = tokens.shape
+        assert seq_len == 1, "Only one token at a time can be processed"
+
+        # (B, Seq_Len) -> (B, Seq_Len, Dim)
+        h = self.tok_embeddings(tokens)
+
+        # Retrieve the pairs (m, theta) corresponding to the positions [start_pos, start_pos + seq_len]
+        freqs_complex = self.freqs_complex[start_pos:start_pos + seq_len]
+
+        # Consecutively apply all the encoder layers
+        for layer in self.layers:
+            h = layer(h, start_pos, freqs_complex)
+        h = self.norm(h)
+        output = self.output(h).float()
+        return output
